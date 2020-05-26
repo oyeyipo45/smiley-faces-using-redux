14a5b54f81fbd4b9ba4afdfe45c33956c9e4823a
Damilola Emmanuel Oyeyipo <oyeyipo45@gmail.com>
2020-05-26 12:23:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. write a reducer
+// 2. state = initial state
+// 3. write the store
+// 4. write action
+// 5. write functiin to display
+// 6. handle click event
+
+const initialState = "(O_O)";
+const face = document.getElementById('face');
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+    case "happy": 
+        return "(X‿X)"
+     case 'sad':
+        return "(X^^X)"
+      case 'angry':
+        return "(x__x)"
+      case 'confused':
+          return  "(X || X)"
+    default:
+        return state
+    }
+}
+
+store = Redux.createStore(rootReducer);
+
+function render() {
+  face.innerHTML = store.getState().toString()
+}
+
+render();
+store.subscribe(render);
+
+
+
+// let getFace = 
+
+document.getElementById("happy").addEventListener("click", () => {
+  store.dispatch({type: "happy"})
+} )
+
+
+document.getElementById("sad").addEventListener("click", () => {
+  store.dispatch({type: "sad"})
+} )
+
+document.getElementById("angry").addEventListener("click", () => {
+  store.dispatch({type: "angry"})
+} )
+
+document.getElementById("confused").addEventListener("click", () => {
+  store.dispatch({type: "confused"})
+} )
+
+
+
